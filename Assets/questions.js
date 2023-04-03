8d8326e0a3c8835814179ae6bf13be6cbcb5b58f
Dylan Hagens <dylan.hagens@gmail.com>
2023-04-03 18:27:14 -0500

    added questions.js

---
+++ Assets/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does .com stand for?",
+    answer: "Dot Commercial",
+    options: [
+        "Dot Commmunity",
+        "Dot Commercial",
+        "Dot Government",
+        "Dot Organization",
+        ]
+    },
+    {
+    numb: 2,
+    question: "What does HTML stand for?",
+    answer: "Hypertext Markup Language",
+    options: [
+        "Hypertext Markup Language",
+        "Huge Markup Language",
+        "Hypertext Mundane Language",
+        "Hyperbole Minimal Language",
+        ]
+    },
+    {
+    numb: 3,
+    question: "What is JavaScript?",
+    answer: "Programming Language",
+    options: [
+        "Script for coffee drinkers",
+        "Just a fancy term used in coding",
+        "Programming Language",
+        "Programming Tool",
+        ]
+    },
+    {
+    numb: 4,
+    question: "What does .org stand for?",
+    answer: "Dot Organization",
+    options: [
+        "Dot Commmunity",
+        "Dot organs",
+        "Dot Government",
+        "Dot Organization",
+        ]
+    },
+    {
+    numb: 5,
+    question: "What is the default font in HTML?",
+    answer: "Times New Roman",
+    options: [
+        "Times New Roman",
+        "Sans-serif",
+        "Ariel",
+        "Comic-Sans",
+        ]
+    },
+]
